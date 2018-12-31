53a0660dc32cbf6f47e77c85c303b4d17855d8e6
i321649 <bruce.li03@sap.com>
2018-12-31 11:28:39 +0800

    table

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import Button from 'antd/lib/button';
 import { Button } from 'antd';
+import { Table, Divider, Tag } from 'antd';
 import 'antd/dist/antd.css';
 //import './index.css';
 import App from './App';
@@ -19,7 +20,7 @@ import * as serviceWorker from './serviceWorker';
 //     </div>,
 //     document.getElementById('root')
 //   );
-
+/* 
 var button1Props = {type: "primary", size: "large"};
 var button1 = React.createElement(Button, button1Props, "Button1");
 
@@ -43,6 +44,65 @@ var button5 = React.createElement(Button, button5Props, "Button5");
 
 var reactElement = React.createElement("div", {}, [button1,button2,button3,button4, button5]);
 
+ReactDOM.render(reactElement, document.getElementById('root'));
+ */
+
+const columns = [{
+  title: 'Name',
+  dataIndex: 'name',
+  key: 'name',
+  render: text => <a href="javascript:;">{text}</a>,
+}, {
+  title: 'Age',
+  dataIndex: 'age',
+  key: 'age',
+}, {
+  title: 'Address',
+  dataIndex: 'address',
+  key: 'address',
+}, {
+  title: 'Tags',
+  key: 'tags',
+  dataIndex: 'tags',
+  render: tags => (
+    <span>
+      {tags.map(tag => <Tag color="blue" key={tag}>{tag}</Tag>)}
+    </span>
+  ),
+}, {
+  title: 'Action',
+  key: 'action',
+  render: (text, record) => (
+    <span>
+      <a href="javascript:;">Invite {record.name}</a>
+      <Divider type="vertical" />
+      <a href="javascript:;">Delete</a>
+    </span>
+  ),
+}];
+
+const data = [{
+  key: '1',
+  name: 'John Brown',
+  age: 32,
+  address: 'New York No. 1 Lake Park',
+  tags: ['nice', 'developer'],
+}, {
+  key: '2',
+  name: 'Jim Green',
+  age: 42,
+  address: 'London No. 1 Lake Park',
+  tags: ['loser'],
+}, {
+  key: '3',
+  name: 'Joe Black',
+  age: 32,
+  address: 'Sidney No. 1 Lake Park',
+  tags: ['cool', 'teacher'],
+}];
+
+var props = {columns: columns, dataSource: data};
+var reactElement = React.createElement(Table, props)
 ReactDOM.render(reactElement, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
